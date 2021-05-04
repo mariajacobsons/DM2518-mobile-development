21627a6fe37d2b1f2e82277ab53da78185921953
Maria Jacobson <maria.jacobsons@icloud.com>
2021-05-04 15:31:49 +0200

    map fix

--- lab1/index.js
+++ lab1/index.js
@@ -3,6 +3,6 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 59.3498092, lng: 18.0684758 },
-    zoom: 8,
+    zoom: 15,
   });
 }
