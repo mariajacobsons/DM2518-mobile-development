92f22044d6ff3dcd39e896eca44441fcabd84ff9
Maria Jacobson <maria.jacobsons@icloud.com>
2021-05-05 15:29:59 +0200

    tilt

--- lab1/index.js
+++ lab1/index.js
@@ -4,5 +4,8 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 59.3498092, lng: 18.0684758 },
     zoom: 15,
+    mapTypeId: "satellite",
+    disableDefaultUI: true,
   });
+  map.setTilt(45);
 }
