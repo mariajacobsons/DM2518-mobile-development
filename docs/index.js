5480c997becb5f152a0eda45912b5fad2a68d0a9
Maria Jacobson <maria.jacobsons@icloud.com>
2021-05-07 15:54:05 +0200

    Geolocation

--- docs/index.js
+++ docs/index.js
@@ -50,6 +50,7 @@ function initMap() {
   initZoomControl(map);
   initPanControl(map);
   setLocation(map);
+  getGeoLocation(map);
 
   marker_drag = new google.maps.Marker({
     map,
@@ -90,6 +91,43 @@ function initMap() {
   nikesApartmentMarker.addListener("click", function () {
     nikesApartmentWindow.open(map, nikesApartmentMarker);
   });
+}
+
+function getGeoLocation(map) {
+  document.querySelector(".buttonHere").onclick = function () {
+    hereWindow = new google.maps.InfoWindow();
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        function (position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude,
+          };
+
+          hereWindow.setPosition(pos);
+          hereWindow.setContent("Min plats");
+          hereWindow.open(map);
+          map.setCenter(pos);
+        },
+        function () {
+          handleLocationError(true, hereWindow, map.getCenter());
+        }
+      );
+    } else {
+      // Browser doesn't support Geolocation
+      handleLocationError(false, hereWindow, map.getCenter());
+    }
+  };
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(
+    browserHasGeolocation
+      ? "Error: The Geolocation service failed."
+      : "Error: Your browser doesn't support geolocation."
+  );
+  infoWindow.open(map);
 }
 
 function toggleBounce() {
